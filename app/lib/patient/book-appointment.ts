ff7777626c4ebc094765c27fd2f5fa95dd8e5e94
Anand Suthar <anandsuthar956@gmail.com>
2025-03-05 23:03:19 +0530

    rely on server component instead of API route handler ⚡

--- app/lib/patient/book-appointment.ts
+++ app/lib/patient/book-appointment.ts
@@ -1,32 +1,104 @@
-export default async function bookAppointment(
-  bookAppointmentData: bookingAppointment,
-  transaction_id: string | null,
-  appointment_charge: string
-): Promise<any> {
-  const endpoint = "/api/patient/appointment";
+import { cache } from "react";
+import { auth } from "../auth";
+import dbConfig from "@utils/db";
+import { Types } from "mongoose";
+import { render } from "@react-email/render";
+import { BookedAppointment, Patient } from "@models/index";
+import sendEmail from "../emails/send-email";
+import { AppointmentBookedTemplate } from "../emails/templates";
+import sendNotification from "../novu";
 
+const bookAppointment = cache(async (bookingData: BookingAppointmentType) => {
   try {
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        ...bookAppointmentData,
-        transaction_id,
-        appointment_charge,
-      }),
-    });
+    const {
+      state,
+      city,
+      hospital,
+      disease,
+      note,
+      transaction_id,
+      appointment_charge,
+    } = bookingData;
 
-    const result = await response.json();
+    const session = await auth();
 
-    if (!response.ok) {
-      throw new Error(result.error?.message || "Failed to book appointment");
+    if (!session) {
+      throw new Error("Unauthorized");
     }
 
-    return result;
-  } catch (error) {
-    console.error("Error booking appointment:", error);
+    const patient_id = new Types.ObjectId(session.user.id);
+    await dbConfig();
+
+    const patient = await Patient.findById(patient_id);
+    if (!patient) {
+      throw new Error("Patient not found");
+    }
+
+    const appointmentData = {
+      state,
+      city,
+      hospital: {
+        id: new Types.ObjectId(hospital.hospital_id),
+        name: hospital.hospital_name,
+      },
+      disease,
+      note,
+      approved: "pending",
+      patient_id: patient._id,
+      doctor_id: null,
+      receptionist_id: null,
+    };
+
+    const res = await BookedAppointment.create(appointmentData);
+    if (!res) {
+      throw new Error("Error saving appointment info");
+    }
+
+    const bookedAppointmentData = {
+      state,
+      city,
+      hospital,
+      disease,
+      note,
+      transaction_id,
+      appointment_charge,
+    };
+
+    // sending email to patient confirming request
+    await sendEmail({
+      to: patient.email || "yourmail@example.com",
+      subject: `Your Appointment Request Has Been Received`,
+      html: render(
+        AppointmentBookedTemplate({
+          name: `${patient.firstname} ${patient.lastname}`,
+          email: patient.email,
+          bookedAppointmentData,
+          transaction_id,
+          appointment_charge,
+        })
+      ),
+      from: {
+        name: "Syncure",
+        address: "support@patientfitnesstracker.com",
+      },
+    });
+
+    // notifying patient
+    await sendNotification(
+      patient._id.toString(),
+      "Your appointment request has been successfully received.",
+      "appointment-request"
+    );
+
+    return JSON.parse(
+      JSON.stringify({
+        message: "Appointment request added successfully",
+      })
+    );
+  } catch (error: any) {
+    console.error("Error adding appointment request:", error);
     throw error;
   }
-}
+});
+
+export default bookAppointment;
