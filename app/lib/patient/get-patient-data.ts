224712617fd2d9ac4a2fe6704398e4b79a9f0156
Anand Suthar <anandsuthar956@gmail.com>
2025-03-05 22:44:53 +0530

    refactor: move get-patient route handler

--- app/lib/patient/get-patient-data.ts
+++ app/lib/patient/get-patient-data.ts
@@ -1,24 +1,32 @@
-import BaseUrl from "@utils/base-url";
+import { cache } from "react";
+import { auth } from "../auth";
+import Patient from "@models/patient";
+import dbConfig from "@utils/db";
+import { Types } from "mongoose";
 
-export default async function getPatientData(): Promise<Patient> {
-  const endpoint = `${BaseUrl}/api/patient`;
-
+const getPatientData = cache(async () => {
   try {
-    const response = await fetch(endpoint, {
-      method: "GET",
-    });
-
-    const result = await response.json();
-
-    if (!response.ok) {
-      throw new Error(result.error?.message || "Failed to fetch patient data");
+    const session = await auth();
+    if (!session?.user?.id) {
+      throw new Error("Unauthorized");
     }
-
-    console.log("data " + result);
-
-    return result;
+    const patient_id = new Types.ObjectId(session.user.id);
+    await dbConfig();
+    const projection = {
+      role: 0,
+      otp: 0,
+      password: 0,
+      current_hospital: 0,
+    };
+    const patientData = await Patient.findById(patient_id, projection);
+    if (!patientData) {
+      throw new Error("Patient not found");
+    }
+    return JSON.parse(JSON.stringify(patientData));
   } catch (error) {
-    console.error("An error occurred while fetching patient data:", error);
+    console.error("Error in getPatientData:", error);
     throw error;
   }
-}
+});
+
+export default getPatientData;
