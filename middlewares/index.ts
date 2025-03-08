5a6c86d5ff5345b4c8522699ed111fcf9d059574
Anand Suthar <anandsuthar956@gmail.com>
2025-03-08 15:37:25 +0530

    chore: remove middlewares

--- middlewares/index.ts
+++
@@ -1,5 +0,0 @@
-import handleExpiredSession from "./handle-expired-session";
-import handlePrivateRoute from "./handle-private-route";
-import handlePublicRoute from "./handle-public-route";
-
-export { handleExpiredSession, handlePrivateRoute, handlePublicRoute };
