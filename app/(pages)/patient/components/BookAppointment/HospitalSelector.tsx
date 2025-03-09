ad8c9433e898b35db8eec7eec3379df5f0f0305a
Anand Suthar <anandsuthar956@gmail.com>
2025-03-09 23:33:03 +0530

    refactor: divide book-appointment component and utilise better error handling for booking appointments

---
+++ app/(pages)/patient/components/BookAppointment/HospitalSelector.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import {
+  Select,
+  SelectItem,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  Spinner,
+} from "@nextui-org/react";
+import useQuery from "@hooks/useQuery";
+import { ChangeEvent } from "react";
+
+export function HospitalSelector({
+  selectedState,
+  selectedCity,
+  selectedHospital,
+  onHospitalChange,
+  isOpenHospitalPopover,
+  setIsOpenHospitalPopover,
+}: HospitalSelectorProps) {
+  const { data: hospitals = [], isLoading } = useQuery<
+    BookAppointmentHospital[]
+  >(
+    selectedState && selectedCity
+      ? `/api/get-hospitals/?state=${selectedState}&city=${selectedCity}`
+      : null
+  );
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = e.target.value;
+
+    const selectedHospitalObj = hospitals.find(
+      (hospital) => hospital.hospital_id === selectedId
+    );
+
+    if (selectedHospitalObj) {
+      // Call the parent's onHospitalChange with the complete hospital object
+      onHospitalChange(selectedHospitalObj);
+    }
+
+    setIsOpenHospitalPopover(false);
+  };
+
+  return (
+    <Popover
+      placement="right"
+      isOpen={isOpenHospitalPopover && !selectedCity}
+      onOpenChange={(open) => setIsOpenHospitalPopover(open)}
+    >
+      <PopoverTrigger>
+        <Select
+          isRequired
+          label="Select Hospital"
+          placeholder="Select your preferred hospital"
+          className="max-w-xs"
+          variant="bordered"
+          value={selectedHospital.hospital_name}
+          onChange={handleChange}
+          disabled={isLoading || !selectedCity}
+          scrollShadowProps={{
+            isEnabled: true,
+          }}
+          endContent={
+            isLoading ? (
+              <Spinner color="primary" size="sm" className="bottom-1/2" />
+            ) : (
+              ""
+            )
+          }
+        >
+          {hospitals.map((item) => (
+            <SelectItem key={item.hospital_id} value={item.hospital_id}>
+              {item.hospital_name}
+            </SelectItem>
+          ))}
+        </Select>
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="px-1 py-2">
+          <div className="text-small font-bold">Please select a city first</div>
+          <div className="text-tiny">
+            You must select a city before selecting a hospital.
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+}
---
+++ app/(pages)/patient/components/BookAppointment/StateSelector.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Select, SelectItem, Spinner } from "@nextui-org/react";
+import useQuery from "@hooks/useQuery";
+
+export function StateSelector({
+  selectedState,
+  onStateChange,
+}: StateSelectorProps) {
+  const { data: states = [], isLoading } = useQuery<string[]>("/api/states");
+
+  return (
+    <Select
+      isRequired
+      label="Select State"
+      placeholder="Select your state"
+      className="max-w-xs"
+      variant="bordered"
+      value={selectedState}
+      onChange={onStateChange}
+      endContent={
+        isLoading ? (
+          <Spinner color="primary" size="sm" className="bottom-1/2" />
+        ) : (
+          ""
+        )
+      }
+      scrollShadowProps={{
+        isEnabled: true,
+      }}
+    >
+      {states.map((item) => (
+        <SelectItem key={item} value={item}>
+          {item}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+}
