3502c5aa551eee4ae639a9ed037c7c3d628977b0
Anand Suthar <anandsuthar956@gmail.com>
2025-03-10 15:40:11 +0530

    refactor: use cache from react and fetch data on server components

--- app/lib/hospital/get-hospital-details.ts
+++ app/lib/hospital/get-hospital-details.ts
@@ -1,23 +1,29 @@
-export default async function getHospitalDetails(): Promise<HospitalDetailsType> {
-  const endpoint = "/api/hospital/additional-details";
+import dbConfig from "@utils/db";
+import { Types } from "mongoose";
+import HospitalDetails from "@models/hospital_details";
+import { cache } from "react";
+import { auth } from "../auth";
 
+const getHospitalDetails = cache(async () => {
   try {
-    const response = await fetch(endpoint, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const session = await auth();
 
-    const result = await response.json();
-
-    if (!response.ok) {
-      throw new Error(
-        result.error?.message || "Failed to fetch hospital details"
-      );
+    if (!session) {
+      throw new Error("Unauthorized");
     }
 
-    return result.data;
+    const hospital_id = new Types.ObjectId(session.user.id);
+
+    await dbConfig();
+
+    const hospitalDetailsData = await HospitalDetails.findOne({
+      hospitalId: hospital_id,
+    });
+
+    if (!hospitalDetailsData) {
+      throw new Error("Hospital details not found");
+    }
+    return JSON.parse(JSON.stringify(hospitalDetailsData));
   } catch (error) {
     console.error(
       "An error occurred while fetching hospital additional details:",
@@ -25,4 +31,6 @@ export default async function getHospitalDetails(): Promise<HospitalDetailsType>
     );
     throw error;
   }
-}
+});
+
+export default getHospitalDetails;
